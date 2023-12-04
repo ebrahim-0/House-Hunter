a41286b37b78a250b2e2c175670b20214e86ac8b
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2023-12-04 23:46:02 +0200

    change some style

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -167,6 +167,7 @@ export default function Hero() {
           }
         >
           <svg
+            className={"w-6 h-6"}
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
